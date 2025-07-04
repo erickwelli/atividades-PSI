b1cb6c3ff0ca7a510dda2567564c355218328a1f
Erick Wellington Dantas de Oliveira Gomes <20231041110005@ifrn.local>
2025-07-04 13:23:17 -0300

    Adicionando arquivo inicial JS

---
+++ jsinicial.js
@@ -0,0 +1,16 @@
+console.log("Olá Mundo!"); //Mostrar texto no terminal
+
+let nome = "Erick" //Criando uma variável que pode ser modificada (let)
+nome = "Feh" //Modificando a variavél
+console.log(nome);
+
+let lista = ["biscoito", "bolacha", "brigadeiro", "chocolate", "morango"]
+console.log(lista[3]);
+
+
+let cor = "Preto"
+cor = "Branco"
+
+if (cor === "Preto") {
+    console.log("É a minha cor")
+}
